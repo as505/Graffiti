e0e95b358333fb7d63ad6ca7aeccf4c2e03adb66
Anders <as505soreide@gmail.com>
2025-04-13 13:25:05 +0200

    adding cookies to track clients

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session, make_response
 from PIL import Image
 import json
+import time
 
 app = Flask(__name__)
 
 image = Image.new("RGB", [1920, 1080], (155, 155, 155))
 
-image.save('static\wall.png', "PNG")
+image.save('static\\wall.png', "PNG")
 
 @app.route('/', methods = ['GET', 'POST'])
 def hello_world():
+    response = make_response(render_template('index.html'))
+    client = request.cookies.get('client_id')
+
+    if client == None:
+        response.set_cookie('client_id', str(time.time()))
+        print("NO COOKIE")
+    else:
+        print(client)
+
     if request.method == 'POST':
         data = request.get_json()
         x = data["x"]
@@ -17,19 +27,19 @@ def hello_world():
 
         draw(x, y)
 
-        return render_template('index.html')
-    
-    elif request.method == 'GET':
-
-        return render_template('index.html')
+    return response
 
 def draw(x, y):
-    with Image.open('static\wall.png') as image:
+    with Image.open('static\\wall.png') as image:
         image.putpixel((x, y), (255, 255, 255))
-        image.save('static\wall.png', "PNG")
+        image.save('static\\wall.png', "PNG")
     return True
 
 @app.after_request
 def add_header(response):
     response.cache_control.max_age = 1
     return response
+
+
+if __name__ == '__main__':
+    app.run()
